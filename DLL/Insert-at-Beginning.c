5376409377ddd5f8ff615179e4e2fcf6371880aa
Shannen Nazareno <nzrnshannen@gmail.com>
2023-01-19 22:47:08 +0800

    added insert-at-beginning

---
+++ DLL/Insert-at-Beginning.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+struct node{
+	int data;
+	struct node *prev_ptr;
+	struct node *next_ptr;
+}*head, *tail, *newNode, *temp;
+
+int n;
+
+void displayList();
+
+int main()
+{
+	int new_data;
+	
+	head=tail=NULL;
+	printf("Enter number of nodes for your linked list: ");
+	scanf("%d", &n);
+	
+	printf("Input %d data for each %d nodes:\n", n);
+	for(int i=1; i<=n; i++)
+	{
+		newNode=(struct node*)malloc(sizeof(struct node));
+		printf("Data %d: ", i);
+		scanf("%d", &newNode->data);
+		newNode->next_ptr = newNode->prev_ptr = NULL;
+		
+		if(head==NULL)
+		{
+			head=tail=newNode;
+		}
+		else
+		{
+			tail->next_ptr=newNode;
+			newNode->prev_ptr=tail;
+			tail=newNode;
+		}
+	}
+	
+	displayList();
+	
+	printf("\n\nEnter data to be inserted at the beginning: ");
+	scanf("%d", &new_data);
+	
+	printf("\n\nInserting node at the beginning...\nPress any key to continue- ");
+	getch();
+	
+	newNode=(struct node*)malloc(sizeof(struct node));
+	newNode->data=new_data;
+	newNode->next_ptr=newNode->prev_ptr=NULL;
+	
+	printf("\n\nBefore insertion: ");
+	displayList();
+	
+	newNode->next_ptr=head;
+	head->prev_ptr=newNode;
+	head=newNode;
+	
+	printf("\nAfter insertion: ");
+	displayList();
+	
+	head=temp=tail=newNode=NULL;
+	
+	free(head);
+	free(temp);
+	free(tail);
+	free(newNode);
+	
+	//will add a function that will release all nodes
+	
+	return 0;
+}
+
+void displayList()
+{
+	temp=head;
+	while(temp!=NULL)
+	{
+		printf("%d ", temp->data);
+		temp=temp->next_ptr;
+	}
+}
