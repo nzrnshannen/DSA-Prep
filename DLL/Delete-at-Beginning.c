c9ec90e57c6b46a035581cb42f54548bd25d3207
Shannen Nazareno <nzrnshannen@gmail.com>
2023-01-31 23:10:54 +0800

    added release_mem function to delete-at-beginning file

--- DLL/Delete-at-Beginning.c
+++ DLL/Delete-at-Beginning.c
@@ -6,8 +6,19 @@ struct node{
 	int data;
 	struct node *next_ptr;
 	struct node *prev_ptr;
-}*head, *tail, *traverse, *newNode, *freeThisNode;
+}*head, *tail, *traverse, *newNode, *freeThisNode, *clear;
 
+void release_mem()
+{
+	clear=head;
+	while(clear!=NULL)
+	{
+		freeThisNode=clear;
+		clear=clear->next_ptr;
+		free(freeThisNode);
+		freeThisNode=NULL;
+	}
+}
 void displayList()
 {
 	traverse=head;
@@ -63,7 +74,8 @@ int main()
 	printf("\nAfter deletion: ");
 	displayList();
 	
-	//would add a function that would delete all nodes
+	release_mem();
+	//code would be redundant:
 	free(head);
 	free(tail);
 	free(newNode);
